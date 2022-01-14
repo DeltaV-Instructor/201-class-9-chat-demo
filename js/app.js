cfc7df8c2a9c6353fddaa03915fda7bb971dc94d
tektechnologies <info@tektechnologies.com>
2022-01-14 13:37:40 -0600

    add js outline

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 'use strict';
 console.log('app.js connected!');
+
+//add global variables to access the DOM
+
+//add constructor function for chat objects
+
+//add prototype method to build chat elements for the render method 
+
+//add handle submit render function 
+
+//add eventlistener to the form submit button 
+
+//add to clear chat list 
