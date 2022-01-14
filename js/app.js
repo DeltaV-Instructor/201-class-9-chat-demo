2153dac5f127ccbdeba26d38cae99cf31588c802
tektechnologies <info@tektechnologies.com>
2022-01-14 14:30:34 -0600

    loggin new comment object

--- js/app.js
+++ js/app.js
@@ -3,32 +3,69 @@ console.log('app.js connected!');
 
 //add global variables to access the DOM
 let chatForm = document.getElementById('chat-form');
-let chatList = document.getElementById('chat-list');
+// let chatList = document.getElementById('chat-list');
 let chatClear = document.getElementById('chat-clear');
 
- 
+
 
 
 //add constructor function for chat objects
 function Comment(userName, text){
-this.userName = userName;
-this.text = text;
+  this.userName = userName;
+  this.text = text;
 }
 
-//add prototype method to build chat elements for the render method 
+//add prototype method to build chat elements for the render method
 
-//add handle submit render function 
+//add handle submit render function
 function handleChatSubmit(event){
-console.log('button click:',event);
-event.preventDefault();
-console.log('form submission values', event.target.who.value);
-console.log('form submission values', event.target.message.value);
+  console.log('button click:',event);
+  event.preventDefault();
+  console.log('form submission values', event.target.who.value);
+  console.log('form submission values', event.target.message.value);
 
 
-//add submission validation
-if(!event.target.who.value || !event.target.message.value){
-  return alert('Looks like we\'ve got another mystery on our hands');
-}
+  //add submission validation
+  if(!event.target.who.value || !event.target.message.value){
+    return alert('Looks like we\'ve got another mystery on our hands');
+  }
+
+
+  let whoCommented = event.target.who.value;
+  let messagePost = event.target.message.value;
+
+  if(whoCommented === 'ScoobyDoo'){
+    messagePost = 'Scooby dooby doo!';
+  }
+  if(whoCommented === 'Shaggy'){
+    messagePost = 'Zoinks!';
+  }
+  if(whoCommented === 'Velma'){
+    messagePost = messagePost.toUpperCase();
+  }
+
+  if(whoCommented === 'Fred'){
+    messagePost = messagePost.toUpperCase();
+  }
+  if(whoCommented === 'Daphne'){
+    messagePost = messagePost.toUpperCase();
+  }
+
+
+  let newComment = new Comment(whoCommented, messagePost);
+  console.log('new comment object',newComment);
+
+
+
+
+
+
+
+
+
+
+
+
 
 }//closes function
 
@@ -49,13 +86,13 @@ if(!event.target.who.value || !event.target.message.value){
 
 
 
-//add SUBMIT EVENT eventlistener to the form submit button 
+//add SUBMIT EVENT eventlistener to the form submit button
 chatForm.addEventListener('submit', handleChatSubmit);
 
 
 
 
-//add CLICK EVENT to clear chat list 
+//add CLICK EVENT to clear chat list
 chatClear.addEventListener('click', function(){
-console.log('clickin on clear chat button!');
+  console.log('clickin on clear chat button!');
 });
